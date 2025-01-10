aa8c030d924148695961192ea75e71a7b65c5f40
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-10 23:55:59 +0530

    Example-1 of sideEffect perform using useEffect Hook and generate random Number by passing randomNumber dependency into useState Hook that changes when the state variable change

--- useEffect Hook/Understand-useEffect-hook/src/App.jsx
+++ useEffect Hook/Understand-useEffect-hook/src/App.jsx
@@ -2,7 +2,7 @@ import Greetings from "./components/Greeting";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App flex items-center justify-center h-screen bg-gray-100">
       <Greetings />
     </div>
   );
--- useEffect Hook/Understand-useEffect-hook/src/components/Greeting.jsx
+++ useEffect Hook/Understand-useEffect-hook/src/components/Greeting.jsx
@@ -5,27 +5,43 @@ const Greetings = () => {
     Math.floor(Math.random() * 5) + 1
   );
 
-  //! Example:- 1
+  const [greetings, setGreetings] = useState("");
+
+  //! Example:- 2
 
   useEffect(() => {
     console.log("Random Greetings!!");
   }, []); // Run only first render
+
+  useEffect(() => {
+    switch (randomNumber) {
+      case 1:
+        setGreetings("Hello");
+        break;
+      case 2:
+        setGreetings("Howdy");
+        break;
+      case 3:
+        setGreetings("Hi");
+        break;
+      case 4:
+        setGreetings("Hey");
+        break;
+      case 5:
+        setGreetings("Yo");
+        break;
+      default:
+        setGreetings("Salutations");
+        break;
+    }
+  }, [randomNumber]);
   return (
     <div className="greeting">
-      <h1>Random Greeting</h1>
-      {randomNumber === 1 ? (
-        <h2 className="message">Hello</h2>
-      ) : randomNumber === 2 ? (
-        <h2 className="message">Howdy</h2>
-      ) : randomNumber === 3 ? (
-        <h2 className="message">Hi</h2>
-      ) : randomNumber === 4 ? (
-        <h2 className="message">Hey</h2>
-      ) : randomNumber === 5 ? (
-        <h2 className="message">Yo</h2>
-      ) : (
-        <h2 className="message">Salutations</h2>
-      )}
+      <h1 className="font-bold text-3xl">Random Greeting</h1>
+      <h2 className="message font-bold text-5xl bg-yellow-400 px-24 mt-5 py-24">
+        {" "}
+        {greetings}
+      </h2>
     </div>
   );
 };
