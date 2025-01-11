60dd1d790ff4e5de5b229fc3cedb44a9bf5bf4d6
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-11 20:58:52 +0530

    Initial Application Setup by creating the components

---
+++ customHooks/src/components/GithubUser.jsx
@@ -0,0 +1,8 @@
+const GithubUser = ({ username }) => {
+  return (
+    <div className="p-4 bg-blue-100 rounded-md shadow-sm text-blue-800 font-medium">
+      {username}
+    </div>
+  );
+};
+export default GithubUser;
--- customHooks/src/main.jsx
+++ customHooks/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import FindUser from "./components/FindUser.jsx";
 
 const appRouter = createBrowserRouter([
   {
@@ -9,8 +10,8 @@ const appRouter = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/about",
-    element: <h1>About Page</h1>,
+    path: "/findUser",
+    element: <FindUser />,
   },
 ]);
 
