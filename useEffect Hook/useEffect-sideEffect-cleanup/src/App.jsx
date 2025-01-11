dd2ae30da2986a978ba8dd1b334b1e4ae83dc312
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-11 16:07:41 +0530

    Add routing with react-router-dom and create About page

--- useEffect Hook/useEffect-sideEffect-cleanup/src/App.jsx
+++ useEffect Hook/useEffect-sideEffect-cleanup/src/App.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import Greetings from "./components/Greeting";
 
 const App = () => {
   return (
     <div className="App flex items-center justify-center h-screen bg-gray-100">
       <Greetings randomMax={8} />
+      <div className="about-page bg-gray-200 p-5 rounded-lg text-center mt-5 cursor-pointer">
+        <Link to={"/about"}>
+          <div>Go to About Page</div>
+        </Link>
+      </div>
     </div>
   );
 };
