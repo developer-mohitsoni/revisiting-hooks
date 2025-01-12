d9f11758127004ed1dedf2588fda1bc607c101c1
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-12 22:47:16 +0530

    Pass familySecret utility to Family component for enhanced message management

--- Context API/src/App.jsx
+++ Context API/src/App.jsx
@@ -1,9 +1,10 @@
 import Family from "./components/Family";
+import { familySecret } from "./utils/familySecret";
 
 const App = () => {
   return (
     <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
-      <Family />
+      <Family secret={familySecret} />
     </div>
   );
 };
