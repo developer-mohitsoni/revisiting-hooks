8aa14c43131c1b51231a14c1d745c87658161754
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-12 22:47:40 +0530

    Update Children component to accept secret prop and display family name

--- Context API/src/components/Children.jsx
+++ Context API/src/components/Children.jsx
@@ -1,12 +1,12 @@
 import GrandDaughter from "./GrandDaughter";
 import GrandSon from "./GrandSon";
 
-const Children = () => {
+const Children = ({ secret }) => {
   return (
     <div className="children border-2 border-orange-500 bg-gray-600 p-4 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold text-orange-300 mb-4">Children</h2>
-      <GrandSon />
-      <GrandDaughter />
+      <h2 className="text-xl font-semibold text-orange-300 mb-4">{`Children ${secret.familyName}`}</h2>
+      <GrandSon secret={secret} />
+      <GrandDaughter secret={secret} />
     </div>
   );
 };
