6950b88c3338dc894b5da28b58d0494905a18e4f
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-12 21:40:30 +0530

    Enhance styling and formatting of MovieDirectorRow component for improved readability

--- Prop Drilling/src/components/MovieDirectorRow.jsx
+++ Prop Drilling/src/components/MovieDirectorRow.jsx
@@ -5,12 +5,13 @@ const MovieDirectorRow = ({ director }) => {
     totalRuntime,
     averageRuntime,
   } = director;
+
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{totalMovies}</td>
-      <td>{totalRuntime}</td>
-      <td>{averageRuntime}</td>
+    <tr className="hover:bg-gray-700 transition">
+      <td className="px-4 py-2">{name}</td>
+      <td className="px-4 py-2">{totalMovies}</td>
+      <td className="px-4 py-2">{totalRuntime}</td>
+      <td className="px-4 py-2">{averageRuntime.toFixed(2)}</td>
     </tr>
   );
 };
