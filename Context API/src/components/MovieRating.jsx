125ceaffab9313ce441c134babc6b14e8815e6eb
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-12 22:11:23 +0530

    Remove unused movie-related components to streamline the codebase

--- Context API/src/components/MovieRating.jsx
+++
@@ -1,9 +0,0 @@
-const MovieRating = ({ rating }) => {
-  return (
-    <p className="text-yellow-400 font-semibold">
-      ⭐ Rating: <span className="text-white">{rating}</span>
-    </p>
-  );
-};
-
-export default MovieRating;
