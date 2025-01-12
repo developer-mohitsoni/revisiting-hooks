0c37654bd8c4cf3e76fe52fb2a0f60384c715993
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-12 14:13:39 +0530

    Replace FindUser component with MovieList in main router configuration

--- Higher-Order-Component/src/main.jsx
+++ Higher-Order-Component/src/main.jsx
@@ -2,16 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import FindUser from "./components/FindUser.jsx";
+import MovieList from "./components/MovieList.jsx";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/findUser",
-    element: <FindUser />,
   },
 ]);
 
