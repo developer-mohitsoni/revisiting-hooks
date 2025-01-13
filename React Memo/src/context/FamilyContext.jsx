3f93b639c89f321c47e90365c18bafe93e252248
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:50:42 +0530

    Remove

--- React Memo/src/context/FamilyContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const FamilyContext = createContext({
-  familyName: "Default Family",
-  onlyParentCanSee: () => "Default Message",
-  onlyGrandchildrenShouldKnow: () => "Default grandchild message",
-});
