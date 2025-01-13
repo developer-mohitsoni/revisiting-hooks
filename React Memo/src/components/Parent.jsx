438a9bfd7d53fdedc9f032439ae375dac4d2fe25
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:57:31 +0530

    Add Parent component with state management and child rendering

---
+++ React Memo/src/components/Parent.jsx
@@ -0,0 +1,13 @@
+const Parent = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+
+      <Child header={`I am a child`} />
+    </div>
+  );
+};
+
+export default Parent;
