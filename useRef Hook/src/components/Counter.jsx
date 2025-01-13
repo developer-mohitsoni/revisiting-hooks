b43f8037b58f0f7ca8f027d5a4b198e22db5c6f9
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 10:35:07 +0530

    Refactor Counter component to use useEffect for incrementing count on render

--- useRef Hook/src/components/Counter.jsx
+++ useRef Hook/src/components/Counter.jsx
@@ -1,17 +1,14 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
+  useEffect(() => {
     setCount((prev) => prev + 1);
-  };
-  return (
-    <>
-      {count}
-      <button onClick={handleClick}>Click Me</button>
-    </>
-  );
+    console.log("Incrementing counter");
+  }); // Increment the count infinite time and page rendering infinite time
+  return <>{count}</>;
 };
 
 export default Counter;
