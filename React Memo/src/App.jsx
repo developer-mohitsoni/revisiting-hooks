92e70c269501b10a6f9d364b7a23cd1c858e6d42
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:58:05 +0530

    Replace Child component with Parent component in App.jsx

--- React Memo/src/App.jsx
+++ React Memo/src/App.jsx
@@ -1,7 +1,9 @@
+import Parent from "./components/Parent";
+
 const App = () => {
   return (
     <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
-      <Child />
+      <Parent />
     </div>
   );
 };
