f93bac5df75b643cd9194cef79f6a8b99583545b
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 10:13:12 +0530

    Refactor app structure to use FamilyContext for managing family data and add new context components for family members using ContextAPI without need to do props drilling to passing props at each level of component

--- Context API/src/App.jsx
+++ Context API/src/App.jsx
@@ -1,11 +1,13 @@
-import Family from "./components/Family";
-import { familySecret } from "./utils/familySecret";
-
+import Family from "./contextComponent/Family";
+import {FamilyContext} from "./context/FamilyContext";
+import { familySecret } from "./utils/familySecret.js";
 const App = () => {
   return (
-    <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
-      <Family secret={familySecret} />
-    </div>
+    <FamilyContext.Provider value={familySecret}>
+      <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
+        <Family />
+      </div>
+    </FamilyContext.Provider>
   );
 };
 
