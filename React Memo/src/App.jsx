f7008b0e8a78fc70260daed0bcfcd88664635337
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:57:42 +0530

    Replace Family component with Child component in App.jsx

--- React Memo/src/App.jsx
+++ React Memo/src/App.jsx
@@ -1,13 +1,8 @@
-import Family from "./contextComponent/Family";
-import {FamilyContext} from "./context/FamilyContext";
-import { familySecret } from "./utils/familySecret.js";
 const App = () => {
   return (
-    <FamilyContext.Provider value={familySecret}>
-      <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
-        <Family />
-      </div>
-    </FamilyContext.Provider>
+    <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
+      <Child />
+    </div>
   );
 };
 
