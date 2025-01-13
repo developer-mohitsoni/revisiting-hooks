646e50d746a15dd7f23d95229a7a0a495cdaa96f
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:14:41 +0530

    Enhance Focus component to include a button for manual input focus and background color change

--- useRef Hook/Example-2_of_useRef-for-DOM-Manipulation/src/components/Focus.jsx
+++ useRef Hook/Example-2_of_useRef-for-DOM-Manipulation/src/components/Focus.jsx
@@ -1,13 +1,25 @@
 import { useEffect, useRef } from "react";
 
 const Focus = () => {
-  const inputElemRef = useRef();
+  const inputElemRef = useRef(null); // DOM element ka reference banaya
 
+  const handleFocus = () => {
+    inputElemRef.current.focus(); // Directly input field pe focus karaya
+    inputElemRef.current.style.backgroundColor = "pink"; // Directly input field ka background color change karya
+  };
+  /*
   useEffect(() => {
-    inputElemRef.current.focus();
-    inputElemRef.current.style.backgroundColor = "pink"
+    inputElemRef.current.focus(); //  Directly input field pe focus karaya
   }, []);
-  return <input type="text" ref={inputElemRef} />;
+  */
+  return (
+    <div>
+      <div>
+        <input ref={inputElemRef} type="text" placeholder="Type something..." className="text-black" />
+      </div>
+      <button onClick={handleFocus}>Focus Input</button>
+    </div>
+  );
 };
 
 export default Focus;
