d4c744f6d501165c4fd6d50f82a9871fb495da9e
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-13 11:57:38 +0530

    Add Child component for rendering header and description

---
+++ React Memo/src/components/Child.jsx
@@ -0,0 +1,15 @@
+const Child = ({ header }) => {
+  return (
+    <div className="child">
+      <h1>{header}</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam magni
+        voluptas quo fugiat aliquam corrupti quisquam fuga mollitia aut illum
+        fugit provident tenetur cumque, neque facilis esse? Delectus, natus
+        laudantium!
+      </p>
+    </div>
+  );
+};
+
+export default Child;
